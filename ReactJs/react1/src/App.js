df0ffc573f3de44cd951882c12382b2c969d6a86
Hammad Sajid <786hammadsajid@gmail.com>
2019-07-22 23:40:12 +0500

    Exclamation

--- ReactJs/react1/src/App.js
+++ ReactJs/react1/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React, and enjoy
+          Learn React, and enjoy!
         </a>
       </header>
     </div>
