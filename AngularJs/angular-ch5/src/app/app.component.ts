68c5a7daa4798f55a2e9384fd36ce19e326af42d
Hammad Sajid <786hammadsajid@gmail.com>
2019-07-30 00:33:42 +0500

    ng tut67 ViewEncapsulation

--- AngularJs/angular-ch5/src/app/app.component.ts
+++ AngularJs/angular-ch5/src/app/app.component.ts
@@ -7,7 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [
-    { type: 'server', name: 'TestServer', content: 'Just a test' }
+    { type: 'server', name: 'TestServer', content: 'Just a test' },
+    { type: 'blueprint', name: 'SecondServer', content: 'Antoher Test' }
   ];
 
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 }
